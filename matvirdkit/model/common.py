fd092f05067146184f2fffc4ac76556abdd1a519
haidi <haidi@hfut.edu.cn>
2022-05-24 20:35:39 +0800

    Add doc for JFData and convert data of DadaFigure to List

--- matvirdkit/model/common.py
+++ matvirdkit/model/common.py
@@ -9,36 +9,66 @@ from pydantic import BaseModel, EmailStr, Field, validator,constr
 from matvirdkit.model.utils import ValueEnum
 
 class JFData(BaseModel):
+     """
+     Json File Data --> JFData. 
+     This class is used to store several types of data
+     file_fmt : str   The file format can be png, jpg / json, yaml / txt  / mp4, avi  / 'txt.gz'
+     file_id  : str   This value will be set when data is inserted into MongoDB GridFS system
+     file_name: str   This file will be saved into GridFS
+     json_id :  str   This value will be set when data is inserted  into MongoDB GridFS system
+     json_file_name: str   If this value is set, then it means the corresponding file will  be  saved into GridFS, and the entry id will be saved in json_id 
+     json_data: dict   If this value is set, the data will be saved directly into the MongoDB in JFData entry. The json_data has priority compared with json_file_name
+
+     1. General txt data. For example, we can save the OUTCAR via following command:
+        JFData(description='This is OUTCAR file',
+               file_fmt = 'txt.gz', 
+               file_id  = '',  
+               file_name= '2eaf9fas0xa23-OUTCAR.gz',
+               json_id  = '', 
+               json_file_name ='ada92fdasl02x.json', 
+               json_data = {} ) 
+                
+     2. Json data type 1.
+        JFData(description='This is json file',
+               file_fmt = 'json', 
+               file_id  = '', 
+               file_name= ''
+               json_id  = '',
+               json_file_name ='ada92fdasl02x.json', 
+               json_data = {} ) 
+
+     3. Json data type 2.
+        JFData(description='This is json file',
+               file_fmt = 'json', 
+               file_id  = '', 
+               file_name= ''
+               json_id  = '',
+               json_file_name ='', 
+               json_data = {'a':1,'b':2} ) 
+
+     4. figure file. Here we only store the figure file and the corresponding data for plotting will not be saved.
+        JFData(description='This is figure file',
+               file_fmt = 'png', 
+               file_id  = '', 
+               file_name= './dataset/bms-1/dos.png'
+               json_id  = '',
+               json_file_name ='', 
+               json_data = {} ) 
+     """
      description : Optional[str]= Field('',description='Data description')
-     # format can be : png, jpg / json, yaml / txt  / mp4, avi 
-     file_fmt: Optional['str'] = Field(None,description='file format of, which will be linked with f_id')
-     file_id: Optional['str'] = Field(None,description='If the file is saved in the mongoDB by file then the corresponding ID will be recorded')
-     file_name: Optional['str'] = Field(None,description='The file name for figure or raw data that will be saved in Mongo File')
-     json_id: Optional['str'] = Field(None,description='If the data is saved in the mongoDB by json then the corresponding ID will be recorded')
-     json_file_name: Optional['str'] = Field(None,description='The file name for json data that will be saved in Mongo directly by ref ID')
-     json_data: Optional[Dict] = Field(None,description='json data that will be saved in current data structure')
+     file_fmt: Optional['str'] = Field('',description='file format of, which will be linked with f_id')
+     file_id: Optional['str'] = Field('',description='If the file is saved in the mongoDB by file then the corresponding ID will be recorded')
+     file_name: Optional['str'] = Field('',description='The file name for figure or raw data that will be saved in Mongo File')
+     json_id: Optional['str'] = Field('',description='If the data is saved in the mongoDB by json then the corresponding ID will be recorded')
+     json_file_name: Optional['str'] = Field('',description='The file name for json data that will be saved in Mongo directly by ref ID')
+     json_data: Optional[Dict] = Field({},description='json data that will be saved in current data structure')
      meta : Any = Field({})
 
 class DataFigure(BaseModel):
     description : Optional[str] = Field('', description='data information')
-    data: JFData = Field(...,description='data')
+    data: List[JFData] = Field(None,description='data')
     figure: JFData = Field(...,description='figure')
     link: str = Field(None)
-
-class JData(BaseModel):
-    description : Optional[str] = ''
-    fmt: str = Field('json', description='data format ')
-    j_id: str = Field(None, description='file name or entry id in mongo DB')
-
-class FData(BaseModel):
-    description : Optional[str] = ''
-    fmt: str = Field('png', description='file format ')
-    f_id: str = Field(None, description='file name or entry id in mongo DB')
-
-class Data(BaseModel):
-    description : Optional[str] = 'figure and data '
-    fdata: List[Dict[str,Tuple[List[JData],List[FData]]]] = Field(None,
-                                      description='A dict record the f_id and corresponding j_id')
 
 class Meta(BaseModel):
       description : Optional[str] = 'Meta information'
@@ -85,25 +115,9 @@ class History(BaseModel):
     """
     History of the material provenance
     """
-
     name: str
     url: str
     description: Optional[Dict] = Field(
         None, description="Dictionary of exra data for this history node"
     )
 
-
-if __name__=='__main__':
-   xfd=Data(fdata=[{"XRD-Cu-Alpha":
-              [
-              [JData(j_id='/home/test/xrd.json')],
-              [
-              FData(f_id='/home/test/XRD.png',fmt='png'),
-              FData(f_id='/home/test/XRD.jpg',fmt='jpg',description='jpg format')
-              ],
-              ]}
-             ]
-                   )
-   print(xfd.json())
-   from monty.serialization import loadfn,dumpfn
-   dumpfn(xfd.dict(),'t.json')
