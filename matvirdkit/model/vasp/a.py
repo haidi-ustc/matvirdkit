29a051e202d7af9f88a59144e78211a209343c37
haidi <haidi@hfut.edu.cn>
2022-06-05 05:27:20 +0800

    rm test function a.py

--- matvirdkit/model/vasp/a.py
+++
@@ -1,6 +0,0 @@
-from matvirdkit.model.vasp.task import TaskDocument as VaspTaskDocument
-import importlib
-
-m='matvirdkit.model.vasp.task'
-m1=importlib.import_module(m,'TaskDocument')
-print(dir(m1))
